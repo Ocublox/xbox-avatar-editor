639092859a5d04eac6a5dbbc1c5da4e87a865cba
SpyMousee <75225729+SpyMousee@users.noreply.github.com>
2025-02-20 12:33:28 -0500

    BAT Update

--- avatar-ssl.xboxlive.bat
+++ avatar-ssl.xboxlive.bat
@@ -3,19 +3,24 @@ title Xbox Avatar Editor
 set CURRENT_DIR=%~dp0
 
 if not exist "%CURRENT_DIR%gmesh.exe" (
-    echo gmesh.exe not found!
-    choice /c yn /m "Would you like to download gmesh.exe?"
+    echo gmesh.exe / meshedit.exe not found!
+    choice /c yn /m "Would you like to download missing files?"
     if errorlevel 2 (
         exit
     ) else (
+        cls
         curl -L "https://github.com/PMZeroSkyline/GPA-Mesh/releases/download/Release/gmesh.exe" -o "%CURRENT_DIR%gmesh.exe"
+        cls
+        curl -L "https://github.com/Ocublox/xbox-avatar-editor/releases/download/Main/meshedit.exe" -o "%CURRENT_DIR%meshedit.exe"
     )
 )
 
 :main
 cls
-
-set /p name=Enter model name (without extension): 
-%CURRENT_DIR%gmesh -s TEXCOORD %name%.csv %name%.obj %name%_build.obj
+echo Make sure the exported OBJ has the same name as the CSV.
+set /p name=Enter model name: 
+%CURRENT_DIR%gmesh -s TEXCOORD %name%.csv %name%.obj %name%_temp.obj
+%CURRENT_DIR%meshedit %name%_temp.obj %name%_build.obj
+del %name%_temp.obj
 pause
 goto main
